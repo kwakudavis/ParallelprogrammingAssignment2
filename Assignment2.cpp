e8eff3acc3057dd64f0ed55d52b790b596b66bf6
Nadia Annor <nadiaannor@Nadias-MacBook-2.local>
2021-05-30 09:04:04 +0200

    first commit

---
+++ Assignment2.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include "jbutil.h"
+
+
+using namespace std;
+
+
+//Main Program
+int main(int argc, char *argv[]){
+
+            cout << "Enter a filename and reload" <<endl;
+
+       return 1;
+
+}
