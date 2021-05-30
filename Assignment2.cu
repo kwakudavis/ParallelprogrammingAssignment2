1cd0b42360f00c24f870ef661211d3a7197f2694
Nadia Annor <nadiaannor@Nadias-MacBook-2.local>
2021-05-30 09:20:47 +0200

    mods

--- Assignment2.cu
+++ Assignment2.cu
@@ -14,11 +14,11 @@ using namespace std;
 
 
 
-__global__ void cuda_hello(int argc, char *argv[]){
+__global__ void cuda_hello(){
     printf("Hello World from GPU!\n");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     cuda_hello<<<1,1>>>();
     return 0;
 }
